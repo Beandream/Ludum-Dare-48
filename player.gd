3bcd79e0a410ca456e14b9c26032899d320e8acf
nwahsadude <nwahsadude@gmail.com>
2021-04-24 14:38:03 -0500

    Add player character

--- player.gd
+++ player.gd
@@ -1,15 +1,30 @@
-extends KinematicBody2D
+extends KinematicBody
 
-var MOTION_SPEED = 250 # Pixels/second.
+const FLOOR_NORMAL = Vector3(0.0, 1.0, 0.0)
 
-func _physics_process(_delta):
-	MOTION_SPEED = 250 
-	var motion = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	motion.y /= 2
-	if (Input.get_action_strength("run") > 0):
-		MOTION_SPEED = MOTION_SPEED * 2
-	motion = motion.normalized() * MOTION_SPEED
-	#warning-ignore:return_value_discarded
-	move_and_slide(motion)
+export var speed := 7.0
+export var gravity := 30.0
+export var jump_force := 12.0
+
+var velocity_y := 0.0
+
+func _physics_process(delta):
+	var direction_ground := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	).normalized()
+	
+	if not is_on_floor():
+		velocity_y -= gravity * delta
+		
+	var velocity = Vector3(
+		direction_ground.x * speed,
+		velocity_y,
+		direction_ground.y * speed
+	)
+	move_and_slide(velocity, FLOOR_NORMAL)
+	
+	if is_on_floor() or is_on_ceiling():
+		velocity_y = 0.0
+		
+	
