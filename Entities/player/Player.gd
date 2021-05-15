5366dc7d1151da2d2b151a4b723073b74991eef2
Bean Dream <beandreamed@gmail.com>
2021-05-15 08:38:38 -0400

    Change the Player Physics. Fixed shaking bug when between floor tiles
    Changing the collision shape to a capsule was probably the fix but I had already reworked the player controller

--- Entities/player/Player.gd
+++ Entities/player/Player.gd
@@ -1,59 +1,54 @@
 extends KinematicBody
 
-const FLOOR_NORMAL = Vector3(0.0, 1.0, 0.0)
-
 export var speed := 7.0
 export var gravity := 30.0
 export var jump_force := 12.0
+export var run_multiplier := 2
 
-var velocity_y := 0.0
+var velocity = Vector3.ZERO
 
 var key = false
 var isPlayer = true
 
-func _ready():
-	pass
-
 func _physics_process(delta):
-	var spd = speed
-	var jmpForce = jump_force
-	$AnimationPlayer.playback_speed = 1
+	var runM = 1
+	var jumpM = 1
 	if (Input.get_action_strength("run") > 0):
-		spd = spd * 2
-		jmpForce = jump_force + jump_force / 5
-		$AnimationPlayer.playback_speed = 4
+		runM = run_multiplier
+		jumpM = run_multiplier * 0.6
+	$AnimationPlayer.playback_speed = runM * runM
+		
 	var direction_ground := Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	).normalized()
 	
-	if not is_on_floor():
-		velocity_y -= gravity * delta
-		
 	if (Input.get_action_strength("jump") && is_on_floor()):
-		velocity_y = jmpForce
-		
-	var velocity = Vector3(
-		(direction_ground.y * spd) + (direction_ground.x * spd),
-		velocity_y,
-		(direction_ground.y * spd) - (direction_ground.x * spd)
+		velocity.y = jump_force * jumpM
+
+	velocity = Vector3(0, velocity.y, 0)
+	
+	velocity += Vector3(
+		(direction_ground.y * speed * runM) + (direction_ground.x * speed * runM),
+		-gravity * delta,
+		(direction_ground.y * speed * runM) - (direction_ground.x * speed * runM)
 	)
-	
-	move_and_slide(velocity, FLOOR_NORMAL)
-	
-	if is_on_floor() or is_on_ceiling():
-		velocity_y = 0.0
+		
+	velocity = move_and_slide(velocity, Vector3.UP)
 		
 	if (self.global_transform.origin.y < -10):
 		get_tree().reload_current_scene()
+		
+	animatePlayer(direction_ground)
 
-	if(direction_ground.x > 0):
+func animatePlayer(dir):
+	if(dir.x > 0):
 		$AnimationPlayer.play("walk")
 		$Position3D/Sprite3D.set_flip_h(false)
-	elif (direction_ground.x < 0):
+	elif (dir.x < 0):
 		$AnimationPlayer.play("walk")
 		$Position3D/Sprite3D.set_flip_h(true)
-	elif (direction_ground.y != 0):
+	elif (dir.y != 0):
 		$AnimationPlayer.play("walk")
 	else:
 		$AnimationPlayer.play("idle")
@@ -67,9 +62,9 @@ func _on_LevelEnd_body_entered(_body):
 		else:
 			get_tree().change_scene("res://main.tscn")
 
-func collect_key(key):
-	$KeyLabel.text += key.get_name() + "\n"
-	self.key = true
+func collect_key(item):
+	$KeyLabel.text += item.get_name() + "\n"
+	key = true
 
 func collect_coin(coin):
 	$KeyLabel.text += coin.get_name() + "\n"
